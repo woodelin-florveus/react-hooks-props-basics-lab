3604a1fe963f7ad523b5f750fd6b9d27402a2bcb
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-20 21:39:35 -0500

    commit tree bf1bb0b99c8a8015a1f30fe81d625ecca2000382

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,22 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+// console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
 
 export default App;
+
+
